805aa014a100fbe30cb132d016577f79b6c20161
Dante Spencer <spencerdantel@gmail.com>
2019-07-18 13:44:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,16 @@
+def speak_to_grandma(speech)
+  if speech == speech
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif
+    speech == speech.upcase
+    return "NO, NOT SINCE 1938!"
+  else 
+    speech == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
+  end
+  
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
